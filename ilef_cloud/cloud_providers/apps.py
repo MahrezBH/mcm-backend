db2e03da0522f310022a3bbf1dcba25839dbab16
MahrezBH <mahrez.benhamad@gmail.com>
2024-06-09 23:04:23 +0100

    first commit in MCM backend

--- ilef_cloud/cloud_providers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CloudProvidersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cloud_providers'
--- ilef_cloud/ilef_cloud/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v0/configurations/', include('configurations.v0.urls')),
-    path('api/v0/', include('cloud_providers.v0.urls')),
-]
