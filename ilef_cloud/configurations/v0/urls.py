7aede2ea412a69f081fe40e8373d9e6e72702783
MahrezBH <mahrez.benhamad@gmail.com>
2024-06-09 23:04:23 +0100

    first commit in MCM backend

--- ilef_cloud/configurations/v0/urls.py
+++
@@ -1,9 +0,0 @@
-# nexus_api/urls.py
-from django.urls import path
-from configurations.views import NexusComponentsView, fetch_secret, store_secret
-urlpatterns = [
-    path('components/', NexusComponentsView.as_view(), name='get_nexus_components'),
-    path('store_secret/', store_secret, name='store_secret'),
-    path('fetch_secret/', fetch_secret, name='fetch_secret'),
-    path('fetch_secret/<str:path>/', fetch_secret, name='fetch_secret'),
-]
