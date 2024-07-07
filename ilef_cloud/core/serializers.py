c2efb9359dc19e699d6a5a39ff2f0a22c4890b4f
MahrezBH <mahrez.benhamad@gmail.com>
2024-07-07 23:33:51 +0100

    Develop the core funcs of MVP

---
+++ ilef_cloud/core/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Add username to the response data
+        data['username'] = self.user.username
+        return data
